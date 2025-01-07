56bca21ec72ad13d89c7dd513a64023751337ba5
yagyadeep <yagyadeepahirwar@gmail.com>
2025-01-07 13:40:32 +0530

    add bookmark route

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import isAuthenticated from "../config/auth.js";
-import { CreateTweet, DeleteTweet, GetAllTweets, GetFollowingTweet, LikeOrDislike } from "../controllers/tweetController.js";
+import { CreateTweet, DeleteTweet, GetAllBookmarkTweet, GetAllTweets, GetFollowingTweet, LikeOrDislike } from "../controllers/tweetController.js";
 const route= express.Router();
 
 route.post("/create",isAuthenticated,CreateTweet);
@@ -8,6 +8,7 @@ route.delete("/delete/:id",isAuthenticated,DeleteTweet)
 route.put("/like/:id",isAuthenticated,LikeOrDislike)
 route.get("/alltweets/:id",isAuthenticated,GetAllTweets);
 route.get("/followingtweets/:id",isAuthenticated,GetFollowingTweet);
+route.post("getbookmark",isAuthenticated,GetAllBookmarkTweet);
 
 
 export default route;
